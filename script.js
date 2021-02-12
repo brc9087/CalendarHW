31748e57cd2181df2272628c76dbb1de8d51061d
Brian Cardines <bcardines811@gmail.com>
2021-02-12 11:46:41 -0500

    readmefiletemplate

--- script.js
+++ script.js
@@ -1,73 +1,48 @@
-$(document).ready(function () {
-    var currentDay = moment().format('dddd MMMM Do YYYY');
-    var hour = moment().hour();
-    console.log(hour);
-    $("#currentDay").append(currentDay).text;
+$(document).ready(function () { 
+    //define variables
+    var currentDayEl = $("#currentday");
+    var saveBtnEl = $(".saveBtn");
+    var textAreaEl = $("textarea");
+    var currentHour = parseInt(moment().format("H"));
+    var todaysDate = moment().format("ddd, " + "MMMM Do YYYY");
+    var plans = JSON.parse(localStorage.getItem("plans")) || [];
 
+    // Current Date display at the top 
+    currentDayEl.text(`${todaysDate}`)
 
-    $("#btn9").on("click", function (event) {
-        event.preventDefault();
-        var user = {
-            input: userInput
-        }
-        var userInput = $(this).siblings("textarea").val();
+    textAreaEl.each(function() {
+        var nameAtt = parseInt($(this).attr("name"));
 
-        localStorage.setItem("input", userInput);
-        var lastInput = localStorage.getItem("input");
-        var JSONFinal = JSON.stringify(lastInput).val();
-        $("#text9").append(JSONFinal.input); }
-        
-
-
- 
-
-
-        // var userInput = $(this).siblings("textarea").val();
-        // localStorage.setItem("input", userInput);
-        // var newInput = localStorage.getItem(userInput);
-        // jsonNewInput = JSON.parse(newInput);
-        // $("textarea").append(jsonNewInput);
-
-        // $("textarea").append(newInput);
-
-
-
-        // function renderLastRegistered() {
-        //     var newInput = localStorage.getItem(userInput);
-        //     $(event).newInput.text;
-        // }
-
-
-
-
-        // localStorage.setItem("userInput", userInput);
-
-        // newInput.text = localStorage.getItem(userInput);
-        // $(".text").innerHTML = localStorage.getItem(userInput)
-        // var lastInput = localStorage.getItem("userInput")
-        // renderLastRegistered();
-
-        // localStorage.getItem("userInput");
-    // function colorCode() {
-    //     var time = moment().hour()
-    //     // console.log(time)
-    //     // if ()
-
-    //     else if ($(".time-block").val() < time) {
-    //         $(".time-block").css("background-color", "blue");
-    //     }
-
-    //     else {
-    //         $(".row").css("background-color", "red");
-    //     }
-    //     colorCode();
-    // }
+        if (currentHour > nameAtt) {
+            $(this).addClass("past");
+        } else if (currentHour < nameAtt) {
+            $(this).addClass("future");
+        } else {
+            $(this).addClass("present");
+        };
     })
 
+    saveBtnEl.on("click", function() {
+        var planEl = $(this).siblings(".description").val();
+        var planTime = $(this).siblings(".description").attr("id");
 
+        var planOb = {
+            text: planEl,
+            time: planTime
+        };
 
+        plans.push(planOb);
+        
+        localStorage.setItem("plans", JSON.stringify(plans));
+    });
 
+    for (var i = 0; i < plans.length; i++) {
+    var currentText = plans[i].text;
+    var currentTime = plans[i].time;
 
+    $(`#${currentTime}`).val(currentText);
+}
 
 
 })
+
