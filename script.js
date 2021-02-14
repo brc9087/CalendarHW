e5cc8236896c4aae05b09a3fa09c6f360bb53ae7
Mona Lisa <bcardines811@gmail.com>
2021-02-14 11:58:29 -0500

    finaltouches

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
-$(document).ready(function () { 
+$(document).ready(function () {
     //define variables
     var currentDayEl = $("#currentDay");
     var currentTimeEl = $("#currentTime")
     var saveBtnEl = $(".saveBtn");
     var textAreaEl = $("textarea");
     var currentHour = parseInt(moment().format("H"));
+    var currentTimePrint = moment().format('LT');
     var todaysDate = moment().format("dddd, " + "MMMM Do YYYY");
     var events = JSON.parse(localStorage.getItem("events")) || [];
 
-    // Current Date display at the top 
-    currentDayEl.text(`${todaysDate}`);
-    currentTimeEl.text(`${currentHour}`)
+    // Current Date display at the top + hour INT
+    currentDayEl.text(todaysDate);
+    currentTimeEl.text(currentTimePrint)
 
+    // uses name attr to change background color for past, present, future
+    textAreaEl.each(function () {
+        var nameTime = parseInt($(this).attr("name"));
 
-    textAreaEl.each(function() {
-        var nameAtt = parseInt($(this).attr("name"));
-
-        if (currentHour > nameAtt) {
+        if (currentHour > nameTime) {
             $(this).addClass("past");
-        } else if (currentHour < nameAtt) {
+        } else if (currentHour < nameTime) {
             $(this).addClass("future");
         } else {
             $(this).addClass("present");
         };
     })
 
-    saveBtnEl.on("click", function() {
+
+
+    //save button saves input information
+    saveBtnEl.on("click", function () {
         var planEl = $(this).siblings(".description").val();
         var planTime = $(this).siblings(".description").attr("id");
 
@@ -35,16 +39,17 @@ $(document).ready(function () {
         };
 
         events.push(planOb);
-        
+
         localStorage.setItem("events", JSON.stringify(events));
     });
 
+    //loops through events and prints current text
     for (var i = 0; i < events.length; i++) {
-    var currentText = events[i].text;
-    var currentTime = events[i].time;
+        var currentText = events[i].text;
+        var currentTime = events[i].time;
 
-    $(`#${currentTime}`).val(currentText);
-}
+        $(`#${currentTime}`).val(currentText);
+    }
 
 
 })
